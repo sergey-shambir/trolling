fb01e2d6d427759996862eeba2164f2b0aa5b4b8
Sergey Shambir <sshambir@yandex.ru>
2019-03-22 01:15:35 +0300

    Fixed return type in TypeScript type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 declare module 'trolling' {
-    type RollbackAction = () => void|Promise<void>;
-    type RollbackFunction = (action: RollbackAction) => void;
+    type AsyncProcedure = () => void|Promise<void>;
+    type RollbackFunction = (action: AsyncProcedure) => void;
     type RolloutCallback = (rollback: RollbackFunction) => void|Promise<void>;
 
-    export function rollout(cb: RolloutCallback): void|Promise<void>;
+    export function rollout(cb: RolloutCallback): AsyncProcedure;
 }
+
